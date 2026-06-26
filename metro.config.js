2e1f8adc8f8976d4de799c2da72a2e3c8993bab1
Uvaish Khan <uvaishkhanwork@gmail.com>
2026-06-27 00:31:39 +0530

    new

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,3 @@
 const { getDefaultConfig } = require('@react-native/metro-config');
-const path = require('path');
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    extraNodeModules: {
-      ...(defaultConfig.resolver.extraNodeModules || {}),
-      'react-native-paper': path.resolve(__dirname, 'node_modules/react-native-paper/lib/commonjs'),
-    },
-  },
-};
+module.exports = getDefaultConfig(__dirname);
